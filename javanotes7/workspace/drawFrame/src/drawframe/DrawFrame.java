bd2c6ae9c939f7eddf8332a2073a2abee9ae92f2
Scott <spfrood@gmail.com>
2019-07-15 16:40:51 -0700

    cleaned thigns up
    cleaned up files

---
+++ javanotes7/workspace/drawFrame/src/drawframe/DrawFrame.java
@@ -0,0 +1,24 @@
+/*
+ * You have entered a maze, twisty little passages 
+ * alike.
+ * 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drawframe;
+
+/**
+ *
+ * @author scott_r_parker
+ */
+public class DrawFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
